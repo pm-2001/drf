61aac52520e50df058482392a61b90ffc3779cd4
pm-2001 <princemauryapm2001@gmail.com>
2022-12-22 23:25:11 +0530

    serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+def student_detail(request,pk):
+    stu = Student.objects.get(id = pk)
+    serializer = StudentSerializer(stu)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type = 'application/json')
+
+def student_list(request):
+    stu = Student.objects.all()
+    serializer = StudentSerializer(stu,many =True)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type = 'application/json')
